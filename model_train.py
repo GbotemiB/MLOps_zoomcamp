782a327b3e09e35faa26141f6db395d8041ff018
Ubuntu <azureuser@mlops.v5gnzt52rf4expf4y4iidvdf1a.cx.internal.cloudapp.net>
2023-08-16 15:59:44 +0000

    model training

--- model_train.py
+++ model_train.py
@@ -3,9 +3,7 @@ import numpy as np
 
 from sklearn.model_selection import train_test_split, KFold
 from lightgbm import LGBMRegressor
-from catboost import CatBoostRegressor
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from sklearn.feature_extraction import DictVectorizer
+from sklearn.metrics import mean_squared_error
 
 import pickle
 import mlflow
@@ -13,225 +11,147 @@ from mlflow.tracking import MlflowClient
 from mlflow.entities import ViewType
 from prepare_features import prepare
 
-MLFLOW_TRACKING_URI = "sqlite:///mlflow.db"
-EXPERIMENT_NAME = 'housing-price'
-
-mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-mlflow.set_experiment(EXPERIMENT_NAME)
-
-data_path = 'data/Housing_dataset_train.csv'
+import optuna
 
 def preprocess(data_path):
     data = pd.read_csv(data_path)
-
-    print(data.columns.tolist())
-    
     data = prepare(data)
-    print("_____________________________________________________________________________")
-    print(data.head())
 
     X = data.drop(columns=['price'], axis=0)
     y = data.price
-
+    print("preprocessing completed")
     return X, y
 
 def split_data(features, label):
     X_train, X_test, y_train, y_test = train_test_split(features, label, test_size=0.2, random_state=42)
+    print("data splitting completed")
     return X_train, X_test, y_train, y_test
 
-def train_model(X_train, X_test, y_train, y_test):
-
-
-with mlflow.start_run():
-
-    params = {
-        'max_depth': 10,
-        'n_estimators': 2000,
-        'learning_rate': 0.002712819361612371,
-        'colsample_bytree': 0.9484547548287134,
-        'subsample': 0.8490126211976283
-        }
-
-    mlflow.log_params(params)
-
-    fold_pred = []
-    splits = 2
-    fold = KFold(n_splits=splits)
-
-    for data_index, test_index in fold.split(X_, y_):
-        X_data, X_test = X_.iloc[data_index], X_.iloc[test_index]
-        y_data, y_test = np.sqrt(y_.iloc[data_index]), y_.iloc[test_index]
-
-        model = LGBMRegressor(**params, objective='rmse')
-        model.fit(X_data, y_data, eval_set=[(X_data, y_data), (X_test, y_test)])
-        model_preds = model.predict(X_test)
-
-        rmse = mean_squared_error(y_test, np.square(model_preds), squared=False)
-        print(f'err: {rmse}')
-        fold_pred.append(rmse)
-
-    RMSE = np.mean(fold_pred)
-
-    mlflow.log_param("splits", splits)
-    mlflow.log_metric("rmse", RMSE)
-    
-    with open('models/lgb.bin', 'wb') as f:
-        pickle.dump(model, f)
-
-    mlflow.log_artifact(local_path="models/lgb.bin", artifact_path="models_pickle")
-    mlflow.lightgbm.log_model(model, artifact_path="models_mlflow")
-
-
-
-
-
-with mlflow.start_run():
-
-    params = {
-        'max_depth': 10,
-        'n_estimators': 2000,
-        'subsample': 0.84,
-        'learning_rate': 0.01,
-        'n_estimators' : 2000
-        }
-
-    mlflow.log_params(params)
-
-    fold_pred_1 = []
-    splits = 10
-    fold = KFold(n_splits=splits)
-
-    for data_index, test_index in fold.split(X_, y_):
-        X_data, X_test = X_.iloc[data_index], X_.iloc[test_index]
-        y_data, y_test = np.sqrt(y_.iloc[data_index]), y_.iloc[test_index]
-
-        model = CatBoostRegressor(**params)
-        model.fit(X_data, y_data, eval_set=[(X_data, y_data), (X_test, y_test)], verbose=0)
-        model_preds = model.predict(X_test)
-
-        rmse = mean_squared_error(y_test, np.square(model_preds), squared=False)
-        print(f'err: {rmse}')
-        fold_pred_1.append(rmse)
-
-    RMSE = np.mean(fold_pred_1)
-
-    mlflow.log_param("splits", splits)
-    mlflow.log_metric("rmse", RMSE)
-    
-    with open('models/cat.bin', 'wb') as f:
-        pickle.dump(model, f)
-
-    mlflow.log_artifact(local_path="models/cat.bin", artifact_path="models_pickle")
-    mlflow.catboost.log_model(model, artifact_path="models_mlflow")
-
-with mlflow.start_run():
-
-    params = {
-        'max_depth': 10,
-        'n_estimators': 2000,
-        'subsample': 0.84,
-        'learning_rate': 0.01,
-        'n_estimators' : 2000
-        }
-
-    mlflow.log_params(params)
-
-    fold_pred_1 = []
-    splits = 10
-    fold = KFold(n_splits=splits)
-
-    for data_index, test_index in fold.split(X_, y_):
-        X_data, X_test = X_.iloc[data_index], X_.iloc[test_index]
-        y_data, y_test = np.sqrt(y_.iloc[data_index]), y_.iloc[test_index]
-
-        model = XGBRegressor(**params)
-        model.fit(X_data, y_data, eval_set=[(X_data, y_data), (X_test, y_test)], verbose=0)
-        model_preds = model.predict(X_test)
-
-        rmse = mean_squared_error(y_test, np.square(model_preds), squared=False)
-        print(f'err: {rmse}')
-        fold_pred_1.append(rmse)
-
-    RMSE = np.mean(fold_pred_1)
-
-    mlflow.log_param("splits", splits)
-    mlflow.log_metric("rmse", RMSE)
-    
-    with open('models/xgb.bin', 'wb') as f:
-        pickle.dump(model, f)
-
-    mlflow.log_artifact(local_path="models/xgb.bin", artifact_path="models_pickle")
-    mlflow.xgboost.log_model(model, artifact_path="models_mlflow")
-
-
-
-
-
-
-# In[ ]:
-
-
-# def objective(trial):
-
-#     max_depth = trial.suggest_int('rf_max_depth', 2, 32)
-#     n_estimators = trial.suggest_int('n_estimators', 100, 4000)
-#     learning_rate = trial.suggest_float('learning_rate', 1e-5, 1e-1, log=True)
-#     colsample_bytree = trial.suggest_float('colsample_bytree', 0, 1)
-#     subsample = trial.suggest_float('subsample', 0, 1)
-
-#     params = {
-#         'max_depth':max_depth,
-#         'colsample_bytree': colsample_bytree,
-#         'learning_rate': learning_rate,
-#         'n_estimators': n_estimators,
-#         'subsample': subsample,
-#     }
-
-#     X_data, X_val, y_data, y_val = data_test_split(X, y, random_state=RANDOM_STATE)
-
-#     LGB = CatBoostRegressor(**params)
-#     LGB.fit(X_data, y_data)
-#     y_pred = LGB.predict(X_val)
-
-#     error = mean_squared_error(y_val, y_pred, squared=False)
-
-#     return error  # An objective value linked with the Trial object.
-
-#  # Invoke optimization of the objective function.
-
-
-# In[ ]:
+def train_model(X_train, y_train, objective):
+    print("model training starting............................")
+    study = optuna.create_study(direction='minimize')  # Create a new study.
+    study.optimize(objective, n_trials=10)
+    print("model training completed")
 
 
 def objective(trial):
 
-    max_depth = trial.suggest_int('rf_max_depth', 2, 16)
-    n_estimators = trial.suggest_int('n_estimators', 100, 4000)
-    learning_rate = trial.suggest_float('learning_rate', 1e-5, 1e-1, log=True)
-#     colsample_bytree = trial.suggest_float('colsample_bytree', 0, 1)
-    subsample = trial.suggest_float('subsample', 0, 1)
+    with mlflow.start_run():
 
-    params = {
-        'max_depth':max_depth,
-#         'colsample_bytree': colsample_bytree,
-        'learning_rate': learning_rate,
-        'n_estimators': n_estimators,
-        'subsample': subsample,
-    }
+        max_depth = trial.suggest_int('rf_max_depth', 2, 16)
+        num_leaves = trial.suggest_int('num_leaves', 2, 30)
+        n_estimators = trial.suggest_int('n_estimators', 100, 4000)
+        learning_rate = trial.suggest_float('learning_rate', 1e-5, 1e-1, log=True)
+        colsample_bytree = trial.suggest_float('colsample_bytree', 0, 1)
+        subsample = trial.suggest_float('subsample', 0, 1)
 
-    X_data, X_val, y_data, y_val = data_test_split(X, y, random_state=RANDOM_STATE)
+        params = {
+            'num_leaves':num_leaves,
+            'max_depth':max_depth,
+            'colsample_bytree': colsample_bytree,
+            'learning_rate': learning_rate,
+            'n_estimators': n_estimators,
+            'subsample': subsample,
+        }
 
-    CAT = CatBoostRegressor(**params)
-    CAT.fit(X_data, y_data, verbose=0)
-    y_pred = CAT.predict(X_val)
+        mlflow.log_params(params)
 
-    error = mean_squared_error(y_val, y_pred, squared=False)
+        fold_pred = []
+        splits = 2
+        fold = KFold(n_splits=splits)
 
-    return error  # An objective value linked with the Trial object.
+        for data_index, test_index in fold.split(X_train, y_train):
+            X_data, X_val = X_train.iloc[data_index], X_train.iloc[test_index]
+            y_data, y_val = np.sqrt(y_train.iloc[data_index]), y_train.iloc[test_index]
 
-study = optuna.create_study(direction='minimize')  # Create a new study.
-study.optimize(objective, n_trials=100)
-study.best_trial
-trial = study.best_trial
-trial.value
-trial.params
+            model = LGBMRegressor(**params, objective='rmse', force_col_wise=True, verbose=0)
+            model.fit(X_data, y_data, eval_set=[(X_data, y_data), (X_val, y_val)])
+            model_preds = model.predict(X_val)
+
+            rmse = mean_squared_error(y_val, np.square(model_preds), squared=False)
+            print(f'err: {rmse}')
+            fold_pred.append(rmse)
+
+        RMSE = np.mean(fold_pred)
+
+        mlflow.log_param("splits", splits)
+        mlflow.log_metric("rmse", RMSE)
+        
+        with open('models/lgb.bin', 'wb') as f:
+            pickle.dump(model, f)
+
+        mlflow.log_artifact(local_path="models/lgb.bin", artifact_path="models_pickle")
+        mlflow.lightgbm.log_model(model, artifact_path="models_mlflow")
+
+    return RMSE
+
+    # study = optuna.create_study(direction='minimize')  # Create a new study.
+    # study.optimize(objective, n_trials=10)
+
+
+def best_run_and_id(EXPERIMENT_NAME):
+
+    experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
+
+    best_run = float('inf')
+    run_id = None
+
+    runs = client.search_runs(
+        experiment_ids=experiment.experiment_id,
+        run_view_type=ViewType.ACTIVE_ONLY,
+        max_results=10,
+        # order_by=["metrics.rmse"]
+    )
+
+    for run in runs:
+        print(run.data.metrics['rmse'])
+        # print(run.info.run_id, run.data.metrics["rmse"])
+        if run.data.metrics['rmse'] < best_run:
+            best_run = run.data.metrics['rmse']
+            run_id = run.info.run_id
+    return best_run, run_id
+
+def register_model(run_id, model_registry_name):
+    mlflow.register_model(
+        model_uri=f"runs:/{run_id}/models",
+        name=model_registry_name
+    )
+    return "model registered successfully"
+
+
+def transition_model(latest_version):
+    client.transition_model_version_stage(
+            name=model_registry_name,
+            version=latest_version,
+            stage="Staging",
+            archive_existing_versions=False,
+        )
+    return "model transition successful"
+
+
+if __name__=="__main__":
+
+    MLFLOW_TRACKING_URI = "sqlite:///mlflow.db"
+    EXPERIMENT_NAME = 'housing-price'
+    model_registry_name = 'housing_price'
+
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+    mlflow.set_experiment(EXPERIMENT_NAME)
+
+    client = MlflowClient(tracking_uri=MLFLOW_TRACKING_URI)
+
+    data_path = 'data/Housing_dataset_train.csv'
+
+    X, y = preprocess(data_path)
+    X_train, X_test, y_train, y_test = split_data(X, y)
+
+    study = optuna.create_study(direction='minimize')  # Create a new study.
+    study.optimize(objective, n_trials=1)
+
+    best_run, run_id = best_run_and_id(EXPERIMENT_NAME)
+    register_model(run_id, model_registry_name)
+
+    latest_version = client.get_latest_versions(name=model_registry_name, stages=['None'])[0].version
+    transition_model(latest_version)
+
+    
