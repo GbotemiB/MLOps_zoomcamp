4affaaabe2f0f73cb0324af33b38ef1f4ccb350b
Gbotemi <gbotemibolarinwa@gmail.com>
2023-08-21 19:27:35 +0000

    making final changes

---
+++ tests/test_model_train.py
@@ -0,0 +1,12 @@
+# import model_train
+
+data_path = "../data/Housing_dataset_train.csv"
+
+
+def test_checking(data_path=data_path):
+    """
+    checking if test is working
+
+    """
+    x = 1
+    assert x == 1
